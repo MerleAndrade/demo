4fd2fa39bb7e16432a9bc93af30cb3d211f8a112
merleandrade <andrade.merle@gmail.com>
2022-12-02 21:04:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Demo</title>
+</head>
+<body>
+
+<h1>
+    <?php
+        $greeting = "Hey";
+
+        echo  "$greeting Felipe";
+    ?>
+</h1>
+
+</body>
+</html>

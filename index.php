795427e4f6876375a33ce3ec1c7498ebc77a3d0d
merleandrade <andrade.merle@gmail.com>
2022-12-05 20:42:36 +0100

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -4,15 +4,34 @@
     <meta charset="UTF-8">
 
     <title>Demo</title>
+
+<style>
+    body {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: sans-serif;
+    }
+</style>
 </head>
 <body>
 
-<h1>
-    <?php
-        $greeting = "Hey";
+<?php
+$name = "Dark Matter";
+$read = true;
 
-        echo  "$greeting Felipe";
-    ?>
+if ($read) {
+    $message = "You have read $name";
+} else {
+    $message = "You have NOT read $name";
+}
+?>
+
+
+<h1>
+<?php echo $message; ?>
+    <?= $message ?>
 </h1>
 
 </body>
